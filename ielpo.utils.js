c7c8dd7e3d7b36c0a92e98cc40dd1f8716ce5f6b
albertoielpo <alberto.ielpo@gmail.com>
2017-09-06 12:01:47 +0200

    Update ielpo.utils.js

--- ielpo.utils.js
+++ ielpo.utils.js
@@ -1,59 +1,58 @@
-IelpoUtils = function() {
-	/*
-	 * Compare two json javascript object
-	 * if forceParseInt = true all values will be cast to int 
-	 */
-	var compareJson = function(obj1, obj2, forceParseInt) { 
-		var ret = {}; 
-		
-		if(forceParseInt){
-			for(var i in obj2) {
-				if(!obj1.hasOwnProperty(i) || parseInt(obj2[i]) !== parseInt(obj1[i])) { 		
-					ret[i] = obj2[i]; 
-				}
+
+/*
+ * Compare two json javascript object
+ * if forceParseInt = true all values will be cast to int 
+ */
+Object.prototype.compareJson = function (obj1, obj2, forceParseInt) {
+	var ret = {}; 
+	
+	if(forceParseInt){
+		for(var i in obj2) {
+			if(!obj1.hasOwnProperty(i) || parseInt(obj2[i]) !== parseInt(obj1[i])) { 		
+				ret[i] = obj2[i]; 
 			}
-			
-		}else{
-			for(var i in obj2) { 	
-				if(!obj1.hasOwnProperty(i) || obj2[i] !== obj1[i]) { 
-					ret[i] = obj2[i]; 
-				} 		
-			} 
 		}
-			
-		return ret; 
-	}; 
+		
+	}else{
+		for(var i in obj2) { 	
+			if(!obj1.hasOwnProperty(i) || obj2[i] !== obj1[i]) { 
+				ret[i] = obj2[i]; 
+			} 		
+		} 
+	}
+		
+	return ret; 
+}; 
 
-	/*
-	 * Return diff between two arrays 
-	 */
-	Array.prototype.diff = function(a) {
-		return this.filter(function(i) {return a.indexOf(i) < 0;});
-	};
+/*
+ * Return diff between two arrays 
+ */
+Array.prototype.diff = function(a) {
+	return this.filter(function(i) {return a.indexOf(i) < 0;});
+};
 
-	/*
-	 * return true if comparer is found into an array
-	 * else false
-	 */
-	Array.prototype.inArray = function(comparer) { 
-		for(var i=0; i < this.length; i++) { 
-			if(comparer(this[i])){
-				return true;
-			} 
-		}
-		return false; 
-	}; 
+/*
+ * return true if comparer is found into an array
+ * else false
+ */
+Array.prototype.inArray = function(comparer) { 
+	for(var i=0; i < this.length; i++) { 
+		if(comparer(this[i])){
+			return true;
+		} 
+	}
+	return false; 
+}; 
 
-	/*
-	 * Push into an Array only if not exists
-	 * example
-	 * * var array = [{ name: "tom", text: "tasty" }];
-	 * * var element = { name: "tom", text: "tasty" };
-	 * * array.pushIfNotExist(element, function(e) { return e.name === element.name && e.text === element.text; });
-	 */
-	Array.prototype.pushIfNotExist = function(element, comparer) { 
-		if (!this.inArray(comparer)) {
-			this.push(element);
-		}
-	};
+/*
+ * Push into an Array only if not exists
+ * example
+ * * var array = [{ name: "tom", text: "tasty" }];
+ * * var element = { name: "tom", text: "tasty" };
+ * * array.pushIfNotExist(element, function(e) { return e.name === element.name && e.text === element.text; });
+ */
+Array.prototype.pushIfNotExist = function(element, comparer) { 
+	if (!this.inArray(comparer)) {
+		this.push(element);
+	}
 };
